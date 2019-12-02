51a32d398a8de3292af79e398170207fcefee229
yassine said <yassine.said@esprit.tn>
2019-12-02 16:40:59 +0100

    menu updated with roles

---
+++ src/app/sidebar/sidebar_items.ts
@@ -0,0 +1,112 @@
+import {Roles} from "../Models/roles"
+
+export const items = [
+    {
+        path: "/dashboard",
+        title: "Dashboard",
+        icon: "menu-icon mdi mdi-television",
+        roles: "All"
+    },
+    {
+        path: "/forms",
+        title: "Form elements",
+        icon: "menu-icon mdi mdi-notification-clear-all",
+        roles: "All"
+    },
+    {
+        path: "/buttons",
+        title: "Buttons",
+        icon: "menu-icon mdi mdi-dna",
+        roles: "All"
+    },
+    {
+        path: "/progressbar",
+        title: "Progress bar",
+        icon: "menu-icon mdi mdi-trackpad",
+        roles: "All"
+    },
+    {
+        path: "/carousel",
+        title: "Carousel",
+        icon: "menu-icon mdi mdi-texture",
+        roles: "All"
+    },
+    {
+        path: "/tooltips",
+        title: "Tooltip",
+        icon: "menu-icon mdi mdi-content-copy",
+        roles: "All"
+    },
+    {
+        path: "/tabs",
+        title: "Tabs",
+        icon: "menu-icon mdi mdi-lightbulb-outline",
+        roles: "All"
+    },
+    {
+        path: "/dropdowns",
+        title: "Dropdown",
+        icon: "menu-icon mdi mdi-backup-restore",
+        roles: "All"
+    },
+    {
+        path: "/sheet",
+        title: "Sheet PFE",
+        icon: "menu-icon fa fa-file-text-o",
+        roles: "All"
+    },
+    {
+        path: "/internship",
+        title: "Internship agreement",
+        icon: "menu-icon fa fa-address-card-o",
+        roles: "All"
+    },
+    {
+        path: "/breadcrumbs",
+        title: "Breadcrumb",
+        icon: "menu-icon mdi mdi-flag-outline",
+        roles: "All"
+    },
+    {
+        path: "/pagination",
+        title: "Pagination",
+        icon: "menu-icon mdi mdi-loupe",
+        roles: "All"
+    },
+    {
+        path: "/tables",
+        title: "Tables",
+        icon: "menu-icon mdi mdi-table",
+        roles: "All"
+    },
+    {
+        path: "/alerts",
+        title: "Alerts",
+        icon: "menu-icon mdi mdi-shield-outline",
+        roles: "All"
+    },
+    {
+        path: "/accordions",
+        title: "Accordion",
+        icon: "menu-icon mdi mdi-altimeter",
+        roles: "All"
+    },
+    {
+        path: "/badges",
+        title: "Badges",
+        icon: "menu-icon mdi mdi-checkbox-multiple-blank-circle-outline",
+        roles: "All"
+    },
+    {
+        path: "/icons",
+        title: "Icons",
+        icon: "menu-icon mdi mdi-chart-bubble",
+        roles: "All"
+    },
+    {
+        path: "/typography",
+        title: "Typography",
+        icon: "menu-icon mdi mdi-format-italic",
+        roles: "All"
+    },
+]
